9a74dbe581a435bad2b9d04259cf9ab17b620161
Varad22310744 <varad.22310744@viit.ac.in>
2025-12-29 23:31:17 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://mini-event-project-fnfhoof7o-varad22310744s-projects.vercel.app"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
